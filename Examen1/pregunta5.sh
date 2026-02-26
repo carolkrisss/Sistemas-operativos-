c679863e186a067d85e8a1411a07304254bc0cc3
carolkrisss <carolkristelg@gmail.com>
2026-02-26 21:45:19 +0000

    archivos examen1

---
+++ Examen1/pregunta5.sh
@@ -0,0 +1,297 @@
+    1  sudo apt update
+    2  sudo apt upgrade
+    3  vim
+    4  sudo apt install vim
+    5  vim
+    6  clear
+    7  ls
+    8  sudo apt install cmake
+    9  python
+   10  sudo apt install vscode
+   11  cd Downloads/
+   12  ls
+   13  sudo apt install code_1.108.1-1768404234_amd64.deb
+   14  sudo apt install ./code_1.108.1-1768404234_amd64.deb 
+   15  ls
+   16  cd ..
+   17  ls
+   18  cd Desktop
+   19  ls
+   20  cd ..
+   21  ls
+   22  Desktop
+   23  cd Desktop
+   24  cd ..
+   25  cd
+   26  mkdir Projects
+   27  ls
+   28  clear
+   29  cd P
+   30  cd Projects 
+   31  touch 
+   32  touch
+   33  touch example.cpp
+   34  ls
+   35  code example.cpp
+   36  gcc
+   37  gcc example.cpp
+   38  code example.cpp
+   39  gcc exampl.cpp
+   40  gcc example.cpp
+   41  cler
+   42  clear
+   43  cd ..
+   44  ls
+   45  cd Projects
+   46  ls
+   47  git clone 
+   48  sudo apt install git
+   49  ls
+   50  cd Projects
+   51  ls
+   52  cd example.cpp
+   53  clear
+   54  git --version
+   55  mkdir ~/Proyects
+   56  ls
+   57  cd ..
+   58  ls
+   59  cd Projects
+   60  git clone https://githubcom/carolkriss/Sistemas-operativoa-.git
+   61  git clone https://github.com/carolkrisss/Sistemas-operativos-.git
+   62  cd carolkrisss/Sistemas-operativos-
+   63  cd Sistemas-operativos
+   64  cd Sistemas-operativos-
+   65  echo "# Mi Proyecto" > README.md
+   66  git init
+   67  git add .
+   68  git commit -m "Initial commit"
+   69  git branch -M main
+   70  git push -u origin main
+   71  cd ..
+   72  ls
+   73  cd
+   74  cd ..
+   75  ls
+   76  cd ..
+   77  ls 
+   78  cd Proyects
+   79  ls
+   80  cd 
+   81  ls
+   82  cd Proyects
+   83  ls
+   84  cd ..
+   85  cd Sistemas-operativos-
+   86  cd 
+   87  cd ..
+   88  ls
+   89  clear
+   90  cd ..
+   91  ls
+   92  find ~ -name ".git" -type d 2>/dev/null
+   93  cd ~/Projects
+   94  ls -la
+   95  cd ~/Projects/Sistemas-operativos-
+   96  touch archivo.cpp
+   97  nano archivo.cpp
+   98  g++ archivo.cpp -o programa
+   99  ./programa
+  100  ls -la ej.txt
+  101  cat ej.txt
+  102  git status
+  103  git add archivo.cpp
+  104  git commit -m "Crear un archivo"
+  105  git add ej.txt
+  106  git commit -m "Agregar archivo ej"
+  107  git config --global user.name "carolkrisss"
+  108  git config --global user.email "carolkristelg@gmail.com"
+  109  git add archivo.cpp
+  110  git commit -m "Agregar archivo"
+  111  git add ej.txt
+  112  git commit -m "Agregar archivo ej"
+  113  git push
+  114  sudo apt update
+  115  sudo apt install g++
+  116  git status
+  117  git add archivo.cpp 
+  118  git commit -m "Agregar archivo"
+  119  git config --global user.name "carolkrisss"
+  120  git config --global user.email "carolkristelg@gmail.com"
+  121  git add ej.txt
+  122  git commit -m "Agregar archivo ej"
+  123  git config --global user.email "carolkristelg@gmail.com"
+  124  git config --global user.name
+  125  git config --global user.gmail
+  126  git config --global user.email
+  127  git add archivo.cpp
+  128  git add ej.txt
+  129  git status
+  130  git add ej.txt
+  131  git config --global user.name
+  132  git config --global user.email
+  133  git branch -a
+  134  git branch -M main
+  135  git add archivo.cpp
+  136  git add ej.txt
+  137  git status
+  138  git add .
+  139  git ls-files --others --exclude-standard
+  140  git config --global user.email "carolkristelg@gmail.com"
+  141  git rm --cached -r .
+  142  git reset
+  143  git add archivo.cpp
+  144  git add ej.txt
+  145  echo "Estado actual"
+  146  git status
+  147  git config --global user.name "carolkrisss"
+  148  git config --global user.email "carolkristelg@gmail.com"
+  149  git config --global --list | grep user
+  150  git add archivo.cpp
+  151  git add ej.txt
+  152  git status
+  153  ls -la
+  154  git add -f archivo.cpp
+  155  git add -f ej.txt
+  156  git status
+  157  git commit-m "programa"
+  158  git commit -m "programa"
+  159  cd ~/Projects/Sistemas-operativos- &&git config --global user.email "carolkristelg@gmail.com
+  160  cd ..
+  161  ls
+  162  Projects
+  163  cd Projects
+  164  ls
+  165  cd Sistemas-operativos-
+  166  ls
+  167  git add archivo.cpp
+  168  git status
+  169  git add programa
+  170  git add .
+  171  git status
+  172  git commit -m "Agregar archivo programa"
+  173  git push
+  174  ls
+  175  g++ archivo.cpp -o programa
+  176  ./programa
+  177  hola
+  178  nano archivo.cpp
+  179  cd ..
+  180  ls 
+  181  cd Projects
+  182  ls
+  183  cd Sistemas-operativos-
+  184  ls 
+  185  cat ej.txt
+  186  ./programa
+  187  cat ej.txt
+  188  cat archivo.cpp
+  189  cat programa
+  190  cat README.md
+  191  exit
+  192  sudo jounalctl
+  193  sudo journalctl
+  194  cd ..
+  195  cd Projects
+  196  cd Sistemas-operativos-
+  197  ls
+  198  code .
+  199  ls
+  200  ls src
+  201  ls include
+  202  ls data
+  203  g++ src/*.cpp -o auditoria -pthread
+  204  ls
+  205  ./auditoria
+  206  pwd
+  207  ls
+  208  sudo apt install tree
+  209  tree
+  210  cd ..
+  211  ls
+  212  cd Projects 
+  213  cd Sistemas-operativos-
+  214  ls
+  215  rm auditoria
+  216  ls
+  217  g++ src/*.cpp -Iinclude -o auditoria -pthread
+  218  ./auditoria
+  219  cd ..
+  220  cd Projects
+  221  cd Sistemas-operativos-
+  222  ls
+  223  rm auditoria
+  224  ls
+  225  g++ src/*.cpp -o auditoria -pthread
+  226  g++ src/*.cpp -Iinclude -o auditoria -pthread
+  227  ls
+  228  rm auditoria 
+  229  ls
+  230  g++ src/*.cpp -Iinclude -o auditoria -pthread
+  231  ./auditoria
+  232  ls
+  233  g++ src/*.cpp -Iinclude -o auditoria -pthread
+  234  ./auditoria
+  235  cd ..
+  236  cd Projects
+  237  cd Sistemas-operativos-
+  238  ls
+  239  rm auditoria
+  240  g++ src/*.cpp -Iinclude -o auditoria -pthread
+  241  ls
+  242  ./auditoria
+  243  exit
+  244  cd ..
+  245  cd Projects
+  246  cd Sistemas-operativos-
+  247  ls
+  248  git status
+  249  git add .
+  250  git commit -m "Menu basico"
+  251  echo "auditoria" >> .gitignore
+  252  git add .gitignore
+  253  git commit -m "agregar gitignore"
+  254  git push
+  255  git status
+  256  git add src/main.cpp
+  257  git commit .m "agregar main"
+  258  it commit -m "agregar main"
+  259  git commit -m "agregar main"
+  260  ls -a
+  261  nano .gitignore
+  262  cd ..
+  263  cd Projects
+  264  cd Sistemas-operativos-
+  265  ls
+  266  git add .
+  267  git commit -m "agregar faltantes"
+  268  git push
+  269  exit
+  270  ccd ..
+  271  cd ..
+  272  cd Projects
+  273  cd Sistemas-operativos-
+  274  ls
+  275  mkdir Examen1
+  276  cd Examen1
+  277  ls 
+  278  touch multithreading.cpp
+  279  ls
+  280  code multithreading.cpp
+  281  g++ multithread.cpp -o multithread -pthread
+  282  git add multithreading.cpp
+  283  git commit -m "Agregado hilos"
+  284  git push
+  285  touch accept.txt
+  286  comando1 && comando2
+  287  ls > accept.txt
+  288  hitory | grep -w ls | wc -1 >> accept.txt
+  289  sudo apt install <deb name>
+  290  ls
+  291  history | grep -w ls | wc -1 >> accept.txt
+  292  history | grep -w ls | wc -l >> accept.txt
+  293  cat accept.txt
+  294  pregunta5.sh
+  295  history pregunta5.sh
+  296  ls
+  297  history > pregunta5.sh
